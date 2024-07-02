1b1d24d8d203427340d8883cac095423e82bb1c5
Alex Bogushenko <bogushenkoas@ukr.net>
2024-07-02 14:27:00 +0300

    1 commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+   enum f{monday = 1, tuesday, wensday, sersday, friday, saturday, sanday};
+
+   cout << friday;
+
+   
+    return 0;
+}
